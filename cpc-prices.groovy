c9689de0ac4e0ad7a38d65f7c1e9000f6b9a53bf
Tim Cooke <tim@trcooke.co.uk>
2018-03-26 08:02:28 +0100

    Added user-agent header to http request to make it look like the request is coming from a browser.

--- cpc-prices.groovy
+++ cpc-prices.groovy
@@ -4,7 +4,7 @@ for (int i = 0; i <= 100; i++) {
   def data
   def code = (i == 100) ? args[0] : args[0] + sprintf("%02d", i)
   try {
-    data = new URL("http://cpc.farnell.com/a/a/a/dp/" + code).getText()
+    data = new URL("http://cpc.farnell.com/a/a/a/dp/" + code).getText(requestProperties: ["User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36", "Accept-Language":"en-US,en;q=0.9"])
     print "o"
   } catch (FileNotFoundException fnfe) { 
     print "."
@@ -15,7 +15,7 @@ for (int i = 0; i <= 100; i++) {
 
   for (int j = 0; j < lines.size(); j++) {
     if (lines.get(j) =~ /<span class="price">/) {
-      prices.put(code, new BigDecimal(lines.get(j + 1).trim().replace('&pound;','')))
+      prices.put(code, new BigDecimal(lines.get(j + 1).trim().replace('£','')))
     }
   }
 }
